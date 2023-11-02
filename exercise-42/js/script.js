f617c0ddd8362465733efce428bdc0104ab56d25
alexsanderlinn <alexsander.linn@icloud.com>
2023-11-02 15:52:15 -0400

    update exercise 42

--- exercise-42/js/script.js
+++ exercise-42/js/script.js
@@ -1,23 +1,23 @@
 function inputname() {
     let name = prompt("Please enter your code");
     if (name === null || name === "" ) {
-        document.getElementById("name").innerHTML = "Inválido";
-        document.getElementById("code").innerHTML = "Inválido";
+        document.getElementById("name").innerHTML = "Aluno: Inválido";
+        document.getElementById("code").innerHTML = "Código: Inválido";
     } else{
         if(name === "1001") { //Aluno: Alexsander Linn//
-            document.getElementById("name").innerHTML = "Alexsander Linn";
-            document.getElementById("code").innerHTML = name;
+            document.getElementById("name").innerHTML = "Aluno: Alexsander Linn";
+            document.getElementById("code").innerHTML = `Código: ${name}`;
         } else {
             if(name === "1002") { //Aluno: João Reis//
-                document.getElementById("name").innerHTML = "João Reis";
-                document.getElementById("code").innerHTML = name;
+                document.getElementById("name").innerHTML = "Aluno: João Reis";
+                document.getElementById("code").innerHTML = `Código: ${name}`;
             } else {
                 if(name === "1003") { //Aluno: Mariana Silva//
-                    document.getElementById("name").innerHTML = "Mariana Silva";
-                    document.getElementById("code").innerHTML = name;
+                    document.getElementById("name").innerHTML = "Aluno: Mariana Silva";
+                    document.getElementById("code").innerHTML = `Código: ${name}`;
                 } else {
-                    document.getElementById("name").innerHTML = "Inválido";
-                    document.getElementById("code").innerHTML = "Inválido";
+                    document.getElementById("name").innerHTML = "Aluno: Inválido";
+                    document.getElementById("code").innerHTML = "Código: Inválido";
                 }
             }
         }
